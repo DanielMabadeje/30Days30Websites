375303d03bd9073c771cbe66e42e9ceaf7add058
Daniel <mabadejedaniel1@gmail.com>
2020-10-08 08:27:01 -0700

    adding function to validate form

--- Day1/script.js
+++ Day1/script.js
@@ -1,1 +1,16 @@
 const input=document.getElementsByTagName('input');
+
+
+function validateform() { 
+    let valid =true
+    // A loop that checks everinput input field in the current tab:
+    for (i = 0; i < input.length; i++) {
+   
+      // If a field is emptinput...
+      if (input[i].value == "") {
+        // add an "invalid" class to the field:
+        input[i].className += " invalid";
+        // and set the current valid status to false
+        valid = false;
+      }
+    }

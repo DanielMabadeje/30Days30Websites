07ed46fc7c432e5dbd4003deb05cbccd16602f0c
Daniel <mabadejedaniel1@gmail.com>
2020-10-12 07:56:17 -0700

    added day 4

---
+++ Day4/config.js
@@ -0,0 +1,13 @@
+"use strict"
+var subscribeUrl = "http://octrace.us14.list-manage.com/subscribe/post?u=b65c95763e0ae8e0c14e7af0e&amp;id=7e9cf89638";
+var email = "mabadejedaniel1@gmail.com";
+var countdown = false;
+var configDate = "2018 01 01";
+var particles = true;
+
+$("#particles-js").backstretch([
+    "./assets/img/green-chameleon-s9CC2SKySJM-unsplash.jpg"
+    , "./assets/img/aaron-burden-y02jEX_B0O0-unsplash.jpg"
+    , "./assets/img/kat-stokes-BoYnwJ8lM54-unsplash.jpg"
+    , ".assets/img/photo-1488190211105-8b0e65b80b4e.jpg"
+], {duration: 3000, fade: 2000});

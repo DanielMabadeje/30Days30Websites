8b5f16f5cd8ba0488912089a3e70b197b68e016f
Daniel <mabadejedaniel1@gmail.com>
2020-11-25 11:51:24 -0800

    added styles for two more classes

--- maserati/assets/js/layout.js
+++ maserati/assets/js/layout.js
@@ -1,1 +1,15 @@
 // This will contain animations ,styling of sidenavs etc
+
+var about = document.getElementById("about-section");
+var sticky = about.offsetTop - 75;
+
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    navbar.classList.remove("not-fixed")
+  } else {
+    navbar.classList.add("not-fixed")
+    navbar.classList.remove("sticky");
+  }
+}

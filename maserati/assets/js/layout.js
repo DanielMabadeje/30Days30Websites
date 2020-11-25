7894bc91d79579cccac2838d0de06976e570f798
Daniel <mabadejedaniel1@gmail.com>
2020-11-25 12:20:50 -0800

    adding event listener scroll to run function

--- maserati/assets/js/layout.js
+++ maserati/assets/js/layout.js
@@ -4,17 +4,38 @@
 var sidenavbar=document.getElementById("sidenav")
 var maincontent=document.getElementById("main");
 var sticky = maincontent.offsetTop - 75;
+// console.log(maincontent.offsetTop)
+// console.log(sticky)
+// console.log(window.pageYOffset)
 
 
 function toggleSidenav() {
   if (window.pageYOffset >= sticky) {
     sidenavbar.classList.add("sidenav-fixed");
     sidenavbar.classList.remove("sidenav-none")
+    console.log(maincontent.offsetTop)
+console.log(sticky)
+console.log(window.pageYOffset)
+console.log('fixed')
   } else {
     sidenavbar.classList.add("sidenav-fixed")
     sidenavbar.classList.remove("sidenav-fixed");
+
+    console.log(maincontent.offsetTop)
+console.log(sticky)
+console.log(window.pageYOffset)
   }
 }
 
+function toggleSidenavColor(param) {
+    if (true) {
+        
+    } else {
+        
+    }
+}
 
-toggleSidenav();
+
+// toggleSidenav();
+
+document.addEventListener("scroll", toggleSidenav)

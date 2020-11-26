db53a4fb7f4d6919880e961957b678466bd7218d
Daniel <mabadejedaniel1@gmail.com>
2020-11-26 03:39:18 -0800

    added script for slick js to run

--- maserati/assets/js/script.js
+++ maserati/assets/js/script.js
@@ -8,3 +8,43 @@ function playAudio(params) {
 function startEngine(params) {
     
 }
+
+
+
+
+
+// slick carousel
+$('.center').slick({
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 2,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 1193,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });

f49c3c13d852b6be835db87c63eab19dc895ab20
Daniel <mabadejedaniel1@gmail.com>
2020-11-26 04:38:00 -0800

    worked on carousel to some extent

--- maserati/assets/js/script.js
+++ maserati/assets/js/script.js
@@ -14,4 +14,40 @@ function startEngine(params) {
 
 
 // slick carousel
-$('.center').slick();
+$('.center').slick(
+    // {
+    //     dots: true,
+    //     infinite: true,
+    //     speed: 300,
+    //     slidesToShow: 3,
+    //     centerMode: true,
+    //     variableWidth: true
+    // }
+
+
+    {
+        centerMode: true,
+  centerPadding: '10',
+  slidesToShow: 2,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+}
+);

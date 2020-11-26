495838065a02c96cf0709c4ed3c944d0ec56d9a8
Daniel <mabadejedaniel1@gmail.com>
2020-11-26 04:02:40 -0800

    editing the layout of carousel

--- maserati/assets/js/script.js
+++ maserati/assets/js/script.js
@@ -14,40 +14,4 @@ function startEngine(params) {
 
 
 // slick carousel
-$('.center').slick(
-//     {
-//     centerMode: true,
-//     centerPadding: '0',
-//     slidesToShow: 1,
-//     responsive: [
-//     //   {
-//     //     breakpoint: 768,
-//     //     settings: {
-//     //       arrows: false,
-//     //       centerMode: true,
-//     //       centerPadding: '10px',
-//     //       slidesToShow: 1
-//     //     }
-//     //   }
-//     //   ,
-//     //   {
-//     //     breakpoint: 1193,
-//     //     settings: {
-//     //       arrows: false,
-//     //       centerMode: true,
-//     //       centerPadding: '40px',
-//     //       slidesToShow: 1
-//     //     }
-//     //   },
-//     //   {
-//     //     breakpoint: 480,
-//     //     settings: {
-//     //       arrows: false,
-//     //       centerMode: true,
-//     //       centerPadding: '40px',
-//     //       slidesToShow: 1
-//     //     }
-//     //   }
-//     ]
-//   }
-);
+$('.center').slick();
